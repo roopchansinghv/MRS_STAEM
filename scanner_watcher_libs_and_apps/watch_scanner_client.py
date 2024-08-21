4348e75d7eaa7b6c55b3e225607741aeb9f098e5
FMRIF real-time software administrator <rtadmin@nitrogen.nimh.nih.gov>
2024-08-21 12:17:59 -0400

    Make state polling method more self-contained and generalizeable, and add ability to store current system state in global python dictionary.

--- scanner_watcher_libs_and_apps/watch_scanner_client.py
+++ scanner_watcher_libs_and_apps/watch_scanner_client.py
@@ -5,31 +5,43 @@
 
 import asyncio
 import requests
+import datetime
 
 
 
-scanner_poll_interval = 1.0   # in seconds
-scanner_state_src_url = 'http://localhost:5000/scanner_state'
+state_poll_interval = 1.0   # in seconds
+state_src_url       = 'http://localhost:5000/scanner_state'
+current_state_dict  = {}
 
 
 
-async def poll_scanner_state():
+async def poll_state(state_url, polling_interval):
 
    while True:
 
-      await asyncio.sleep(scanner_poll_interval)
+      await asyncio.sleep(polling_interval)
 
       try:
 
-         state = requests.get(scanner_state_src_url)
+         # get date and time at which scanner state is polled
+         current_state_check_date_time = datetime.datetime.now().strftime("%Y_%m_%d_%H:%M:%S")
 
+         # poll URL where state is published to
+         state = requests.get(state_url)
+
+         # Convert published JSON struct to Python dictionary
          data = state.json()
-         print(data['scanner AE Title'] + " is in state " + data['scanner_state'] + " and occurred at time: " + data['scanner_last_event_time'])
-         print(data)
+         # print(type(data))
+         # print(data)
+         # Extract desired information from packet
+         current_state_dict = data['all_events']
+         print (str(current_state_dict))
+         print('   \n' + data['scanner AE Title'] + " is in state " + data['scanner_state'] + " and occurred at time: " + data['scanner_last_event_time']
+               + ' detected at ' + current_state_check_date_time + '\n')
 
       except:
 
-          print("Couldn't reach URL: %s to determine scanner state." % scanner_state_src_url)
+          print("Couldn't reach URL: %s to determine scanner state." % state_url)
 
 
 
@@ -38,7 +50,7 @@ if __name__ == "__main__":
    loop = asyncio.get_event_loop()
 
    try:
-      asyncio.ensure_future(poll_scanner_state())
+      asyncio.ensure_future(poll_state(state_src_url, state_poll_interval))
       loop.run_forever()
    except KeyboardInterrupt:
       pass
