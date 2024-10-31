e46ecc834ec4f42169dcfa4cf7ca4ee5e8819318
V. R. <for_usenet@yahoo.com>
2024-10-31 17:08:54 -0400

    Reformat data frame of scanner events, where each column of data represents a vendor's specific set of events, and the row labels are the generic events.  Should be able to actually fill out the tables after this.

--- scanner_watcher_libs_and_apps/scanner_events.py
+++ scanner_watcher_libs_and_apps/scanner_events.py
@@ -19,6 +19,18 @@ events  = [['00_Generic', '00_GEHC', '00_Siemens'],
            ['01_Generic', '01_GEHC', '01_Siemens'],
            ['02_Generic', '02_GEHC', '02_Siemens']]
 
-scanner_events_df = pandas.DataFrame.from_records(events, index=None, columns=vendors)
+base_df = pandas.DataFrame.from_records(events, index=None, columns=vendors)
+
+# Now, take data frame of generic and vendor-specific scan events, and make a new
+# data frame, with the vendors as the column labels, and the generic scan events
+# as the row keys/labels.
+
+scanner_events_df = pandas.DataFrame(base_df.loc[:, vendors[1:]].to_numpy(), index=base_df['Generic'].values.tolist(), columns=vendors[1:])
+
+# Check each vendor's list of events
+print(scanner_events_df['GEHC'])
+print(scanner_events_df['Siemens'])
+
+# and entire new data frame
 print(scanner_events_df)
 
